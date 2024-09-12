e4318a4480f24c0f4c7c6b75e2a2d0a9965af3ca
Maya <maya.husic@gmail.com>
2024-09-12 09:40:29 -0600

    Initial Commit

---
+++ CosmikDebris/CosmikDebrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmikDebrisApp.swift
+//  CosmikDebris
+//
+//  Created by Maya McPherson on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CosmikDebrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
